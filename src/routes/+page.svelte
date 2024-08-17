2797a1acb0378b87310259886ce9d26660cd8f3e
yoelbuzgalo <yoelbuzgalo@gmail.com>
2024-08-17 23:13:35 +0300

    Removed typewriter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-    import { TypeWriter } from "svelte-typewrite";
     import { fly } from "svelte/transition";
     import { onMount } from "svelte";
     import ScrollableSection from "$lib/components/ScrollableSection.svelte";
@@ -18,24 +17,23 @@
 
 <div id="home-slot" class="h-full w-full">
     <ScrollableSection sectionName="home-section" nextSectionName="about-section">
-        <article class="grid grid-cols-9 my-5 h-full w-full">
-            <div class="col-span-4">
-                <h1 class="text-xl w-fit">Hi there! My name is <strong>Yoel B. Buzgalo</strong></h1>
-                <h1 class="text-xl w-fit">and I'm a ...</h1>
+        <article class="grid grid-rows-9 h-full w-full">
+            <div class="row-span-6 flex justify-center items-center h-full w-full my-5">
+                {#if isVisible}
+                <img class="framed h-full w-fit object-contain" transition:fly="{{y:100, duration: 1000}}" src="/images/tlv1.jpg" alt="Yoel Buzgalo sitting in Tel Aviv with laptop"/>
+                {/if}
+            </div>
+            <div class="row-span-3 flex w-full justify-center items-center flex-row">
+                <h1 class="text-xl w-fit">Hi there! My name is <strong>Yoel B. Buzgalo</strong> and I am a&nbsp;</h1>
                 <div class="w-fit bg-black bg-opacity-50 rounded-sm text-xl">
-                    <TypeWriter texts={["Software Engineer", "Practical Electrical Engineer"]} waitBetweenTexts=1000/>
+                    <h1>Software Engineer</h1>
                     <div class="green-line"></div>
                 </div>
-            </div>
-            <div class="col-span-1 justify-center items-center"></div>
-            <div class="col-span-4 justify-center items-center">
-                {#if isVisible}
-                <img class="framed lg:max-w-lg  md:max-sm" transition:fly="{{y:100, duration: 1000}}" src="/images/tlv1.jpg" alt="Yoel Buzgalo sitting in Tel Aviv with laptop"/>
-                {/if}
             </div>
         </article>
     </ScrollableSection>
     <ScrollableSection sectionName="about-section" nextSectionName={null}>
+        <h1 class="text-xl underline mb-5">Timeline Overview:</h1>
         <Timeline timelineData={data.timeline}/>
     </ScrollableSection>
 </div>
